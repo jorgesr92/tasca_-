29b7b3fbc4e3601e5f97b37e5870d7653b11ca3d
jorgesr92 <jr_6924@hotmail.com>
2018-02-19 14:36:33 +0100

    commint 5

---
+++ js/calculohipoteca.js
@@ -0,0 +1,11 @@
+function calulohipoteca() {
+    var fecini=document.getElementById("fechaInicio").value;
+    var capital=document.getElementById("Euros").value;
+    var meses=document.getElementById("Meses").value;
+    var interesas=document.getElementById("intereses").value;
+    var resultats=100;
+    return "Pagaràs una hipoteca de "+resultats+"€ mensuals ";
+
+    document.getElementById("resultats").innerHTML="Pagaràs una hipoteca de "+resultats+"€ mensuals ";
+
+}
